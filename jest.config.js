399845f57ffa6854b1f996dbb5e3d43ac497dc13
Nybble Core <nybblecore@gmail.com>
2022-12-10 11:25:14 +0530

    Fixed unit tests

--- jest.config.js
+++ jest.config.js
@@ -2,42 +2,43 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage/ui',
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: ['js', 'json', 'jsx'],
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['<rootDir>/enzyme.config.js'],
-  
-  "moduleNameMapper": {
-    ".+\\.(svg|png|jpg|mp3|m4a)$": "identity-obj-proxy",
-    "^.+\\.(css|less|scss)$": "babel-jest"
-  },
-  // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/(*.)(test).js?(x)'],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['\\\\node_modules\\\\','<rootDir>/swishqube-core','<rootDir>/dist'],
-
-  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-  testURL: 'http://localhost',
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-    '/node_modules/(?!@babel\/runtime)'
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
   ],
-  
-  // Indicates whether each individual test should be reported during the run
-  verbose: false,
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', 
+    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/config/jest/babelTransform.js',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { outputDirectory: 'coverage/ui', outputName: 'junit.xml' },
+    ],
+  ],
+  transformIgnorePatterns: [
+    // '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+    "/node_modules/(?!gsap|@mui|d3|d3-array|d3-tip|internmap|delaunator|robust-predicates)"
+  ],
+  modulePaths: [],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    "\\.(css|less)$": 'identity-obj-proxy',
+  },
+  moduleFileExtensions: ['web.js', 'js', 'web.ts', 'ts', 'web.tsx', 'tsx', 'json', 'web.jsx', 'jsx', 'node'],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  resetMocks: true,
 };
